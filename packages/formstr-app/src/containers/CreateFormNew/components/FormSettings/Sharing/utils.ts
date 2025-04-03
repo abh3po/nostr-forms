0f04216e9fec1337a6a50449eaea7653502553d5
devAmaresh <amprasad.2003@gmail.com>
2025-04-03 13:58:52 +0530

    removed redundant checks

--- packages/formstr-app/src/containers/CreateFormNew/components/FormSettings/Sharing/utils.ts
+++ packages/formstr-app/src/containers/CreateFormNew/components/FormSettings/Sharing/utils.ts
@@ -1,9 +1,6 @@
 import { nip19 } from "nostr-tools";
 
 export const isValidNpub = (npub: string) => {
-  if (!npub.startsWith("npub1") || npub.length !== 63) {
-    return false;
-  }
   try {
     const decoded = nip19.decode(npub);
     return decoded.type === "npub"; // Ensure it’s an npub

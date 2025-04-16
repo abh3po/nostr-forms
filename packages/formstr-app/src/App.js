83c5d8286584946326fbfa79a61f6b95d5f4f4ea
Ramakrishna <16651792+rramaa@users.noreply.github.com>
2025-04-16 13:10:19 +0000

    removed hash router

--- packages/formstr-app/src/App.js
+++ packages/formstr-app/src/App.js
@@ -1,12 +1,21 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Routing from "./components/Routing";
 import { ProfileProvider } from "./provider/ProfileProvider";
 import { ApplicationProvider } from "./provider/ApplicationProvider";
 
 function App() {
+  // to maintain backward compatibility with older urls, we copy the hash to pathname and rerender the application
+  // the search was coming as part of the pathname, so needed to split the search part
+  if (window.location.hash) {
+    const hashValue = window.location.hash.replace("#", "");
+    const [route, search] = hashValue.split("?");
+    window.location.pathname = route;
+    window.location.search = search;
+    window.location.hash = "";
+  }
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
         <ApplicationProvider>
           <ProfileProvider>
@@ -14,7 +23,7 @@ function App() {
           </ProfileProvider>
         </ApplicationProvider>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 

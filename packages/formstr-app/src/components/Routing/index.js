c38840c33cc85b41214b4097b02eff1270266140
Abhay Raizada <toabhayraizada@gmail.com>
2025-08-19 11:06:35 +0530

    Remove OLD Routes

--- packages/formstr-app/src/components/Routing/index.js
+++ packages/formstr-app/src/components/Routing/index.js
@@ -51,10 +51,6 @@ function Routing() {
         element={withNewCreateFormHeaderWrapper(EditForm)}
       />
       <Route
-        path={`${ROUTES.EDIT_FORM_SECRET_OLD}/*`}
-        element={withNewCreateFormHeaderWrapper(EditForm)}
-      />
-      <Route
         path={`${ROUTES.PUBLIC_FORMS}/*`}
         element={withNostrHeaderWrapper(PublicForms)}
       />
@@ -74,10 +70,6 @@ function Routing() {
       />
       <Route
         path={`${ROUTES.RESPONSES_SECRET}/*`}
-        element={withNostrHeaderWrapper(Response)}
-      />
-      <Route
-        path={`${ROUTES.RESPONSES_SECRET_OLD}/*`}
         element={withNostrHeaderWrapper(Response)}
       />
       <Route
